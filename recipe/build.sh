441b8b9de459fd9a6317782bbf7ac792725bcbee
Maksim Rakitin <mrakitin@bnl.gov>
2019-08-28 16:58:01 -0400

    Get rid of build.sh and bld.bat

--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-if [[ ${HOST} =~ .*darwin.* ]]; then
-	$PYTHON -m pip install . --no-deps --ignore-installed -vv --global-option "build" --global-option "--no-openmp"
-else
-	$PYTHON -m pip install . --no-deps --ignore-installed -vv
-fi
